6e6320ded77b975cc32b9d2f55606bcf6bbedc99
evoup <evoex123@gmail.com>
2015-05-14 15:47:44 +0800

    update

--- FMP_api_php/modules/publish.m
+++ FMP_api_php/modules/publish.m
@@ -13,177 +13,157 @@
   +----------------------------------------------------------------------+
  */
 use FacebookAds\Api;
-Api::init($app_id, $app_secret, $access_token);
-
-/**
- * Step 1 Read the AdAccount (optional)
- */
-use FacebookAds\Object\AdAccount;
-use FacebookAds\Object\Fields\AdAccountFields;
-
-
-$account = (new AdAccount($account_id))->read(array(
-  AdAccountFields::ID,
-  AdAccountFields::NAME,
-  AdAccountFields::ACCOUNT_STATUS
-));
-
-// Check the account is active
-if($account->{AdAccountFields::ACCOUNT_STATUS} !== 1) {
-  throw new \Exception(
-    'This account is not active');
-}
-
-/**
- * Step 2 Create the AdCampaign
- */
+use FacebookAds\Object\AdImage;
+use FacebookAds\Object\Fields\AdImageFields;
+use FacebookAds\Object\AdCreative;
+use FacebookAds\Object\Fields\AdCreativeFields;
+use FacebookAds\Object\Fields\ObjectStorySpecFields;
+use FacebookAds\Object\Fields\ObjectStory\LinkDataFields;
+use FacebookAds\Object\Fields\ObjectStory\AttachmentDataFields;
+use FacebookAds\Object\CustomAudience;
+use FacebookAds\Object\Fields\CustomAudienceFields;
+use FacebookAds\Object\Values\CustomAudienceTypes;
 use FacebookAds\Object\AdCampaign;
 use FacebookAds\Object\Fields\AdCampaignFields;
 use FacebookAds\Object\Values\AdObjectives;
+use FacebookAds\Object\TargetingSpecs;
+use FacebookAds\Object\Fields\TargetingSpecsFields;
+use FacebookAds\Object\AdSet;
+use FacebookAds\Object\Fields\AdSetFields;
+use FacebookAds\Object\Fields\AdGroupBidInfoFields;
+use FacebookAds\Object\Values\BidTypes;
+use FacebookAds\Object\AdGroup;
+use FacebookAds\Object\Fields\AdGroupFields;
 
-$campaign  = new AdCampaign(null, $account->id);
+Api::init($app_id, $app_secret, $access_token);
+echo $account_id;
+
+$page_id = "463893297084514";
+//$pixel_id = "";
+
+// Upload Images
+echo "Uploading Images";
+
+for($i = 1; $i <= 3; $i++) {
+  $image[$i] = new AdImage(null, $account_id);
+  $image[$i]->{AdImageFields::FILENAME} = "/tmp/africa/africa{$i}.jpg";
+  echo ".";
+  $image[$i]->create();
+}
+
+echo "\n";
+
+// Create Multi Product Ad Creative
+
+$child_attachments = array();
+
+for($i = 1; $i <= 3; $i++) {
+  $child_attachments[] = array(
+    AttachmentDataFields::LINK => 'http://photos.vigyaan.com/africa'.$i,
+    AttachmentDataFields::IMAGE_HASH => $image[$i]->hash,
+  );
+}
+
+// The ObjectStorySpec helps bring some order to a complex
+// API spec that assists with creating page posts inline.
+
+$object_story_spec = array(
+  ObjectStorySpecFields::PAGE_ID => $page_id,
+  ObjectStorySpecFields::LINK_DATA => array(
+    LinkDataFields::MESSAGE => 'Check out some pictures from Africa',
+    LinkDataFields::LINK => 'http://photos.vigyaan.com',
+    LinkDataFields::CAPTION => 'photos.vigyaan.com',
+    LinkDataFields::CHILD_ATTACHMENTS => $child_attachments,
+));
+
+$creative = new AdCreative(null, $account_id);
+
+$creative->setData(array(
+  AdCreativeFields::NAME => 'Africa Creative 1',
+  AdCreativeFields::OBJECT_STORY_SPEC => $object_story_spec,
+));
+
+$creative->create();
+$creative_id = $creative->id;
+echo 'Creative ID: '.$creative_id ."\n";
+
+// Create Audience
+// Learn more about custom audiences here
+// https://developers.facebook.com/docs/marketing-api/custom-audience-website
+
+//$audience = new CustomAudience(null, $account_id);
+//$audience->setData(array(
+  //CustomAudienceFields::NAME => 'f8 demo custom audience',
+  //CustomAudienceFields::DESCRIPTION => 'people who visited our website',
+  //CustomAudienceFields::SUBTYPE => 'WEBSITE',
+  //CustomAudienceFields::RETENTION_DAYS => 30,
+  //CustomAudienceFields::PREFILL => true,
+  //CustomAudienceFields::RULE => array(
+    //'url' => array(
+      //'i_contains' => ''
+    //)),
+  //'pixel_id' => $pixel_id,
+//));
+
+//$audience->create();
+//$audience_id = $audience->id;
+//echo "Audience ID: " . $audience_id."\n";
+
+// Create a campaign
+// A campaign can have one or more adsets
+
+$campaign  = new AdCampaign(null, $account_id);
 $campaign->setData(array(
-  AdCampaignFields::NAME => $_SESSION[__SESSION_CAMP_EDIT]['step1']['campaignName'],
-  AdCampaignFields::OBJECTIVE => AdObjectives::WEBSITE_CLICKS,
-  //AdCampaignFields::OBJECTIVE => AdObjectives::MOBILE_APP_INSTALLS,
+  AdCampaignFields::NAME => 'F8 Demo Campaign',
+  AdCampaignFields::OBJECTIVE => AdObjectives::WEBSITE_CONVERSIONS,
   AdCampaignFields::STATUS => AdCampaign::STATUS_PAUSED,
 ));
 
 $campaign->validate()->create();
+$campaign_id = $campaign->id;
+echo "Campaign ID: " . $campaign_id . "\n";
 
-/**
- * Step 3 Search Targeting
- */
-use FacebookAds\Object\TargetingSearch;
-use FacebookAds\Object\Search\TargetingSearchTypes;
-
-$results = TargetingSearch::search(
-  $type = TargetingSearchTypes::INTEREST,
-  $class = null,
-  $query = 'facebook');
-
-// we'll take the top result for now
-$target = (count($results)) ? $results->current() : null;
-$target=null;
-
-echo "Using target: ".$target->name."\n";
-
-use FacebookAds\Object\TargetingSpecs;
-use FacebookAds\Object\Fields\TargetingSpecsFields;
+// Create targeting
 
 $targeting = new TargetingSpecs();
 $targeting->{TargetingSpecsFields::GEO_LOCATIONS}
 = array(
     'countries' => explode('|',$_SESSION[__SESSION_CAMP_EDIT]['step3']['location'])
 );
-$targeting->{TargetingSpecsFields::INTERESTS} = array(
-  'id' => $target->id,
-  'name' => $target->name
-);
+//$targeting->{TargetingSpecsFields::CUSTOM_AUDIENCES} = array(
+  //'id' => $audience_id);
 
-/**
- * Step 4 Create the AdSet
- */
-use FacebookAds\Object\AdSet;
-use FacebookAds\Object\Fields\AdSetFields;
-use FacebookAds\Object\Fields\AdGroupBidInfoFields;
-use FacebookAds\Object\Values\BidTypes;
+// Create an ad set
+// An adset can have one or more ad units
 
-$adset = new AdSet(null, $account->id);
+$adset = new AdSet(null, $account_id);
 $adset->setData(array(
-  AdSetFields::NAME => 'Test AdSet',
-  AdSetFields::CAMPAIGN_GROUP_ID => $campaign->id,
+  AdSetFields::NAME => 'My First AdSet',
+  AdSetFields::CAMPAIGN_GROUP_ID => $campaign_id,
   AdSetFields::CAMPAIGN_STATUS => AdSet::STATUS_ACTIVE,
-  AdSetFields::DAILY_BUDGET => $_SESSION[__SESSION_CAMP_EDIT]['step4']['budget']*100,
+  AdSetFields::DAILY_BUDGET => '15000',
   AdSetFields::TARGETING => $targeting,
-  AdSetFields::BID_TYPE => BidTypes::BID_TYPE_CPM,
-  AdSetFields::BID_INFO =>
-    array(AdGroupBidInfoFields::IMPRESSIONS => 2),
-  AdSetFields::START_TIME =>
-    (new \DateTime($_SESSION[__SESSION_CAMP_EDIT]['step4']['schedule_start']))->format(\DateTime::ISO8601),
-  AdSetFields::END_TIME =>
-    (new \DateTime($_SESSION[__SESSION_CAMP_EDIT]['step4']['schedule_end']))->format(\DateTime::ISO8601)
+  //AdSetFields::BID_TYPE => BidTypes::BID_TYPE_ABSOLUTE_CPM,
+  //AdSetFields::BID_INFO => array(AdGroupBidInfoFields::IMPRESSIONS => 2),
+  AdSetFields::BID_TYPE => BidTypes::BID_TYPE_ABSOLUTE_OCPM,
+  AdSetFields::BID_INFO => array('ACTIONS' => 3000),
+  //AdSetFields::PROMOTED_OBJECT => array('pixel_id' => $pixel_id),
 ));
 
 $adset->validate()->create();
-echo 'AdSet  ID: '. $adset->id . "\n";
+$adset_id = $adset->id;
+echo 'AdSet ID: '. $adset_id . "\n";
 
-/**
- * Step 5 Create an AdImage
- */
-use FacebookAds\Object\AdImage;
-use FacebookAds\Object\Fields\AdImageFields;
+// Create an ad
+// Each ad can only have one creative
 
-$image = new AdImage(null, $account->id);
-echo $picLocationArr[0];
-
-$image->{AdImageFields::FILENAME}
-  ='/tmp/FB-f-Logo__blue_512.png';
-  //= 'http://54.175.164.231/fmpapi1.0/get/images/05/96/dde5ba8bb2c36eaa08cf5b9035bbae34.jpg';
-
-
-$image->create();
-echo 'Image Hash: '.$image->hash . "\n";
-
-
-/**
- * Step 5 Create an AdCreative
- */
-use FacebookAds\Object\AdCreative;
-use FacebookAds\Object\Fields\AdCreativeFields;
-
-$creative = new AdCreative(null, $account->id);
-$creative->setData(array(
-  AdCreativeFields::NAME => 'Sample Creative',
-  AdCreativeFields::TITLE => "title",
-  AdCreativeFields::BODY => "body",
-  AdCreativeFields::IMAGE_HASH => $image->hash,
-  AdCreativeFields::OBJECT_URL => "http://www.baidu.com/" 
-));
-
-$creative->create();
-echo 'Creative ID: '.$creative->id . "\n";
-// 测试再加一个adcreative
-$creative2 = new AdCreative(null, $account->id);
-$creative2->setData(array(
-  AdCreativeFields::NAME => 'Sample Creative2',
-  AdCreativeFields::TITLE => "title2",
-  AdCreativeFields::BODY => "body2",
-  AdCreativeFields::IMAGE_HASH => $image->hash,
-  AdCreativeFields::OBJECT_URL => "http://www.google.com/" 
-));
-
-$creative2->create();
-echo 'Creative2 ID: '.$creative2->id . "\n";
-
-/**
- * Step 7 Create an AdGroup
- */
-use FacebookAds\Object\AdGroup;
-use FacebookAds\Object\Fields\AdGroupFields;
-
-$adgroup = new AdGroup(null, $account->id);
+$adgroup = new AdGroup(null, $account_id);
 $adgroup->setData(array(
-  AdGroupFields::CREATIVE =>
-    array('creative_id' => $creative->id),
+  AdGroupFields::CREATIVE => array('creative_id' => $creative_id),
   AdGroupFields::NAME => 'My First AdGroup',
-  AdGroupFields::CAMPAIGN_ID => $adset->id,
-  AdGroupFields::ADGROUP_STATUS => 'ACTIVE'
+  AdGroupFields::CAMPAIGN_ID => $adset_id,
 ));
 
 $adgroup->create();
-echo 'AdGroup ID:' . $adgroup->id . "\n";
-
-$adgroup2 = new AdGroup(null, $account->id);
-$adgroup2->setData(array(
-  AdGroupFields::CREATIVE =>
-    array('creative_id' => $creative2->id),
-  AdGroupFields::NAME => 'My Second AdGroup',
-  AdGroupFields::CAMPAIGN_ID => $adset->id,
-  AdGroupFields::ADGROUP_STATUS => 'ACTIVE'
-));
-
-$adgroup2->create();
-echo 'AdGroup2 ID:' . $adgroup2->id . "\n";
-
-
+$adgroup_id=$adgroup->id;
+echo 'AdGroup ID: ' . $adgroup_id . "\n";
